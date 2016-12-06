f5a7315926a487632ca25f2cea77e4cce26d6dd9
Wyatt Lindsey <wyatt.b.lindsey@gmail.com>
2016-12-06 15:03:41 -0800

    implement random number file naming

--- server/app/controllers/upload.js
+++ server/app/controllers/upload.js
@@ -1,7 +1,20 @@
 const uuid = require('node-uuid');
 
-
 module.exports = (req, res) => {
+  if (!req.files) {
+    res.send('No files were uploaded');
+    return;
+  }
 
+  const file = req.files.file;
+  const fileName = uuid.v4();
 
+  file.mv(`server/uploads/originals/${fileName}.obj`, (err) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send(err);
+    } else {
+      res.sendStatus(201);
+    }
+  });
 };
--- server/app/server.js
+++ server/app/server.js
@@ -10,25 +10,7 @@ const port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, '../dist')));
 
-app.post('/upload', fileUpload(), (req, res) => {
-  if (!req.files) {
-    res.send('No files were uploaded');
-    return;
-  }
-
-  req.files.file.mv(`server/uploads/originals/AS12_1.obj`, (err) => {
-    if (err) {
-      console.error(err);
-      res.status(500).send(err);
-    } else {
-      res.sendStatus(201);
-    }
-
-
-  });
-});
-
-
+app.post('/upload', fileUpload(), upload);
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
